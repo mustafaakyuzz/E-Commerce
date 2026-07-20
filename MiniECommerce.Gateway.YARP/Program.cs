157e0bbcbdfc33e33d7b416f18194f3e949d53ab
Mustafa Akyuz <akyuzmustafaa@hotmail.com>
2026-07-20 22:59:03 +0300

    chore: API Gateway for local is added

---
+++ MiniECommerce.Gateway.YARP/Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors();
+
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapReverseProxy();
+
+app.Run();
