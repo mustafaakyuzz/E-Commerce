618e413ffbf7339276b7d30709329aa6557b9da4
Mustafa Akyuz <akyuzmustafaa@hotmail.com>
2026-07-21 19:42:33 +0300

    chore: JWT Authentication is added to gateway

---
+++ MiniECommerce.Gateway.YARP/Context/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniECommerce.Gateway.YARP.Models;
+
+namespace MiniECommerce.Gateway.YARP.Context
+{
+    public sealed class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
